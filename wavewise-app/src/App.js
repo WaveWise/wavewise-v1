6a21a5d5bf921b0f86aad12b9a8155a5c260716b
Your Name Here <hankm4@gmail.com>
2018-10-01 13:26:47 -0400

    initial commit

---
+++ wavewise-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>Hello Darkness my old friend</div>
+    )
+  }
+}
+
+export default App
