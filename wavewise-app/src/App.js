dea0075d5a56d8908324ac2b873a42dd7b5b8610
Your Name Here <hankm4@gmail.com>
2018-10-01 22:07:48 -0400

    add placeholder image for home screen

--- wavewise-app/src/App.js
+++ wavewise-app/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './App.css'
+import ncOutline from './ncOutline.gif'
+
+// import Canvas from './Canvas'
 
 class App extends Component {
   render () {
     return (
-      <div>
+      <div className='container'>
         <h1 className='header'>WaveWise</h1>
+        <img className='nc-photo' src={ncOutline} />
+        <div className='more-info-link'>
+          <p>more info</p>
+        </div>
       </div>
     )
   }
