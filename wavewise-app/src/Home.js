3dd0bfb0dfa173a9115d7e17f0c7f5bb8946ccce
Your Name Here <hankm4@gmail.com>
2018-10-03 11:31:39 -0400

    convert img to svg

--- wavewise-app/src/Home.js
+++ wavewise-app/src/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
+import CoastSVG from './assets/CoastSVG'
 
-import Coast from './assets/Coast@2x.png'
 import wavewise from './assets/wavewise-logo_0.5x.png'
 
 class Home extends Component {
@@ -15,7 +15,7 @@ class Home extends Component {
     return (
       <div className='container'>
         <h1 className='header'><img className='logo' src={wavewise} />Wave<strong>Wise</strong></h1>
-        <img className='coastline' src={Coast} />
+        <CoastSVG className='coastline' />
         <div className='more-info-link'>
           {this.props.locations.SurfCity.map((status, i) =>
             <div className='suggestion-body'>
---
+++ wavewise-app/src/assets/CoastSVG.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SvgComponent = props => (
+  <svg viewBox='0 0 1920 1080' {...props}>
+    <title>WhiteCoast</title>
+    <path
+      d='M1344.53 131C1326.45 145.37 1065 318 1065 318s-122.14 79.73-199.87 151.4C796.49 544.1 742 624.85 691.51 713.68c-38.36 70.66-84.72 162.73-116.08 235.2'
+      fill='none'
+      stroke='#c6d8d3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={38.36}
+    />
+  </svg>
+)
+
+export default SvgComponent
