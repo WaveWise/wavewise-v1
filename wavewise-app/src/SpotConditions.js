f0d474b9b090a994820f3eee593ea3c9f8b01e34
Kari Scharf <karischarf@gmail.com>
2018-10-05 11:38:59 -0400

    added component

---
+++ wavewise-app/src/SpotConditions.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+import Home from './Home'
+import data from './data'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+
+class SpotConditions extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      spots: {
+        
+    }
+  }
+
+export default SpotConditions 
