3f1d3eb808c3a440c706896676a52be125e08457
Your Name Here <hankm4@gmail.com>
2018-10-08 12:54:40 -0400

    checking out

--- wavewise-app/src/index.js
+++ wavewise-app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Router } from '@reach/router'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
