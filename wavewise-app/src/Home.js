6e30879a24774a9e374e280001dc347a61415304
Your Name Here <hankm4@gmail.com>
2018-10-08 15:52:42 -0400

    last minute before check in

--- wavewise-app/src/Home.js
+++ wavewise-app/src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CoastSVG from './assets/CoastSVG'
 import Menu from './Menu'
+import image from './assets/map_invert.png'
 
 import wavewise from './assets/wavewise-logo_0.5x.png'
 
@@ -12,7 +13,7 @@ class Home extends Component {
         <h1 className='header'><img className='logo'
           alt='Sorry Surfer - Not Found'
           src={wavewise} />Wave<strong className='wise'>Wise</strong></h1>
-        <CoastSVG className='coastline' />
+        <img className='coastline' src={image} />
         <div className='surf-suggestion'>
           <h3 className='suggestion'>Surf now at {bestSpot.spot_name}!</h3>
           <p className='surf-condition'>The surf is {bestSpot.swell_height_ft}ft at {bestSpot.swell_period_s} seconds.</p>
