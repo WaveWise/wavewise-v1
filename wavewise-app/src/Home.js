3e3be4f16e78298dedb217e26839e11fac1beb1c
Your Name Here <hankm4@gmail.com>
2018-10-11 11:09:15 -0400

    style homepage

--- wavewise-app/src/Home.js
+++ wavewise-app/src/Home.js
@@ -20,6 +20,9 @@ class Home extends Component {
         <h1 className='header'><img className='logo'
           alt='Sorry Surfer - Not Found'
           src={wavewise} />Wave<strong className='wise'>Wise</strong></h1>
+        <div className='surf-suggestion'>
+          <h3 className='suggestion'>Surf now at {bestSpot.spot_name}!</h3>
+        </div>
         {bestSpot.spot_name === 'Wrightsville Beach - South End'
           ? <Link
             to={`/spots/${bestSpot.spot_name}/${bestSpot.tide_type}/${bestSpot.tide_time}/${bestSpot.swell_direction}/${bestSpot.swell_height_ft}/${bestSpot.swell_period_s}/${bestSpot.wind_speed_mph}/${bestSpot.wind_direction}/${bestSpot.spot_id}/${bestSpot.rating}`}
@@ -60,11 +63,8 @@ class Home extends Component {
             />
           </Link>
           : <div />}
-        <div className='surf-suggestion'>
-          <h3 className='suggestion'>Surf now at {bestSpot.spot_name}!</h3>
-          <p className='surf-condition'>The surf is {bestSpot.swell_height_ft}ft at {bestSpot.swell_period_s} seconds.</p>
-          <p className='overview-conditions'>The wind is {bestSpot.wind_direction} at {bestSpot.wind_speed_mph} mph</p>
-        </div>
+        <p className='surf-condition'>The surf is {bestSpot.swell_height_ft}ft at {bestSpot.swell_period_s} seconds.</p>
+        <p className='overview-conditions'>The wind is {bestSpot.wind_direction} at {bestSpot.wind_speed_mph} mph</p>
         <div className='footer'>
           <BottomDrawer spots={this.props.spots} />
           <p>Check out other spots</p>
